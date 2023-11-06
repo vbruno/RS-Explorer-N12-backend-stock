d3ac53cd97c4d69b958a2e15197b7553e62f6338
Bruno S Velho <bruno.velho@gmail.com>
2023-11-06 10:45:46 -0300

    Validando Usuário

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,10 +1,16 @@
 const { Router } = require('express');
+
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated');
+
 const UsersController = require('../controllers/UsersController');
+const UsersValidatedController = require('../controllers/UsersValidatedController');
 
 const usersRoutes = Router();
 
 const usersController = new UsersController();
+const usersValidatedController = new UsersValidatedController();
 
 usersRoutes.post('/', usersController.create);
+usersRoutes.get('/validated', ensureAuthenticated, usersValidatedController.index);
 
 module.exports = usersRoutes;
